980bc0c0a35fe84836116a04525081142a9b7329
Finn Banks <banks.finn@gmail.com>
2020-03-01 14:13:54 -0500

    Updates

---
+++ precache-manifest.da948cc61e2f858234abf428664fea4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986b517fff77c57266fe438fafc689d2",
+    "url": "/spotify-chronological/index.html"
+  },
+  {
+    "revision": "f10ff3874b7977c988f6",
+    "url": "/spotify-chronological/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "90f4e943bb54f08fff38",
+    "url": "/spotify-chronological/static/js/2.c9027a50.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/spotify-chronological/static/js/2.c9027a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10ff3874b7977c988f6",
+    "url": "/spotify-chronological/static/js/main.c4e0fa57.chunk.js"
+  },
+  {
+    "revision": "f4393c765b35affbdd27",
+    "url": "/spotify-chronological/static/js/runtime-main.ac0642a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-chronological/static/media/logo.5d5d9eef.svg"
+  }
+]);
