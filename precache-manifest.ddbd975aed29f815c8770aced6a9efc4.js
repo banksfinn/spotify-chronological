6e8bddda1d9ed2cc54152acf614709c1805e7faa
Finn Banks <banks.finn@gmail.com>
2020-03-02 12:46:39 -0500

    Updates

--- precache-manifest.ddbd975aed29f815c8770aced6a9efc4.js
+++ precache-manifest.ddbd975aed29f815c8770aced6a9efc4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "35b5e9a61b155c3eb929f664c86f1ebf",
+    "revision": "9a06dab110eb3b19a884a58401621ff9",
     "url": "/spotify-chronological/index.html"
   },
   {
-    "revision": "126cf8f6f4d32cd506b3",
+    "revision": "64be136451960a601ca0",
     "url": "/spotify-chronological/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "e2c21b0eae0166ac31e9",
-    "url": "/spotify-chronological/static/js/2.7bf419d9.chunk.js"
+    "revision": "475e144328a4328b47e4",
+    "url": "/spotify-chronological/static/js/2.a5691e23.chunk.js"
   },
   {
     "revision": "49c45e15b22906ea61e3b3085d227f58",
-    "url": "/spotify-chronological/static/js/2.7bf419d9.chunk.js.LICENSE.txt"
+    "url": "/spotify-chronological/static/js/2.a5691e23.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "126cf8f6f4d32cd506b3",
-    "url": "/spotify-chronological/static/js/main.63dab508.chunk.js"
+    "revision": "64be136451960a601ca0",
+    "url": "/spotify-chronological/static/js/main.7a6d277e.chunk.js"
   },
   {
     "revision": "f4393c765b35affbdd27",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-chronological/precache-manifest.874fc424bbd0f623ff0512032805e683.js"
+  "/spotify-chronological/precache-manifest.ddbd975aed29f815c8770aced6a9efc4.js"
 );
 
 self.addEventListener('message', (event) => {
