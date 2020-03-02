f3b495f072007be7412fa1068368af3da63bfa2d
Finn Banks <banks.finn@gmail.com>
2020-03-01 22:46:54 -0500

    Updates

--- precache-manifest.186141b886d29314e5e370b9dcda8f4b.js
+++ precache-manifest.186141b886d29314e5e370b9dcda8f4b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9964ab24a9acd1e8ee433a4533dbe6be",
+    "revision": "78b55fd4be08775cba691f672e719e02",
     "url": "/spotify-chronological/index.html"
   },
   {
-    "revision": "c80056ca960c13fc6ed4",
+    "revision": "54dab9905fd631782e06",
     "url": "/spotify-chronological/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spotify-chronological/static/js/2.e2117e86.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c80056ca960c13fc6ed4",
-    "url": "/spotify-chronological/static/js/main.a96f7131.chunk.js"
+    "revision": "54dab9905fd631782e06",
+    "url": "/spotify-chronological/static/js/main.a88ce7d1.chunk.js"
   },
   {
     "revision": "f4393c765b35affbdd27",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-chronological/precache-manifest.23574924ef4608ed8b811aa7fd7a0505.js"
+  "/spotify-chronological/precache-manifest.186141b886d29314e5e370b9dcda8f4b.js"
 );
 
 self.addEventListener('message', (event) => {
