f678a42c21cfa334cd9e3a74ab3b75a7fe1bdc0f
Finn Banks <banks.finn@gmail.com>
2020-03-01 23:01:14 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-chronological/precache-manifest.e2dd8c0d392a884ad425d699b78cb457.js"
+  "/spotify-chronological/precache-manifest.dc5eb9175fa64d1c0d98e93b0eba6701.js"
 );
 
 self.addEventListener('message', (event) => {
