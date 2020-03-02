a76829a23e151407fbcc27a0401d6c27e37f201e
Finn Banks <banks.finn@gmail.com>
2020-03-01 19:00:41 -0500

    removed client secret (its been refreshed don't worry)

--- src/SpotifyComponents.js
+++ src/SpotifyComponents.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
+
 export class SpotifyAPI extends React.Component {
 
 
-
-
     async get_access() {
-        console.log(process.env.REACT_APP_AUTH);
         const pre_request = {
             method: "POST",
             headers: {
-                'Content-Type' :'application/x-www-form-urlencoded',
-                'Authorization': process.env.REACT_APP_AUTH,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + process.env.REACT_APP_AUTH,
             }
         };
         pre_request.body = "grant_type=client_credentials";
         let access_token = "";
         await fetch('https://accounts.spotify.com/api/token', pre_request).then((response) => console.log(response.json()));//.then((responseData) => {access_token=responseData['access_token']});
-        console.log(access_token);
+        console.log(access_token + " is the access token");
     }
 
     render() {
         return (
-            <Button style={{margin: '2% 10% 2% 10%'}} onClick={() => {this.get_access()}}>
+            <Button style={{margin: '2% 10% 2% 10%'}} onClick={() => {
+                this.get_access()
+            }}>
                 <Button.Content>Send Request</Button.Content>
             </Button>
         )
